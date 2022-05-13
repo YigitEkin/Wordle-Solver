7c7cca8fef2470a2a1e6087599e9f8baca99fa1e
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2022-05-13 23:17:54 +0300

    transform to js

--- words.js
+++ words.js
@@ -16277,3 +16277,33 @@ export const words = [
   "zulus",
   "zunis",
 ];
+
+export const filterByOccurrenceAtIndex = (
+  index: number,
+  char: string,
+  words: string[]
+) => {
+  return words.filter((word) => {
+    return word.charAt(index) === char;
+  });
+};
+
+export const filterByNonOccurrenceAtIndex = (
+  index: number,
+  char: string,
+  words: string[]
+) => {
+  return words.filter((word) => {
+    return word.charAt(index) !== char;
+  });
+};
+
+export const filterByIncludesWithWrongIndex = (
+  index: number,
+  char: string,
+  words: string[]
+) => {
+  return words.filter((word) => {
+    return word.includes(char) && word.charAt(index) !== char;
+  });
+};
